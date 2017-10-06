87ae06e9d5bcab20df368f2e8985e0a0998bf489
Daniel Park <dsp2109@gmail.com>
2017-10-06 11:29:45 -0400

    Shiny Dashboard folder using Shu code from NYDS demo

---
+++ ShinyDashboard/global.R
@@ -0,0 +1,6 @@
+# convert matrix to dataframe
+state_stat <- data.frame(state.name = rownames(state.x77), state.x77)
+# remove row names
+rownames(state_stat) <- NULL
+# create variable with colnames as choice
+choice <- colnames(state_stat)[-1]
