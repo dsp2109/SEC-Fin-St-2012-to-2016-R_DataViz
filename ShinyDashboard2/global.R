959ede3a55ba482a517ecd8653ea8689ffe3f435
Daniel Park <dsp2109@gmail.com>
2017-10-10 07:03:39 -0400

    fixed charts finally

--- ShinyDashboard2/global.R
+++ ShinyDashboard2/global.R
@@ -9,3 +9,15 @@ library(dplyr)
 load("Data/shiny_sec_data.rda")
 load("Data/ggfilings.rda")
 rm(state_stats)
+
+choice = unique(nums_shiny$AD_Desc)
+fin_choice = unique(nums_shiny$fin_line)
+
+
+ggplot(ggdata2 %>% filter(last10k %in% 2012:2015)) + 
+  geom_col(aes(x= last10k, y = instances)) + 
+  ggtitle("Companies no longer filing")
+
+print(class(gg_dt))
+print(ls())
+print(getwd())
--- ShinyDashboard2/ui.R
+++ ShinyDashboard2/ui.R
@@ -1,38 +1,53 @@
 
 
-shinyUI(dashboardPage(skin = "black",
+shinyUI(dashboardPage(
                       dashboardHeader(title = "SEC Financial St Data"),
+                      
                       dashboardSidebar(
                         
-                        sidebarUserPanel("SEC Data by Year and Industry",
+                        sidebarUserPanel("Fin St by: Yr, Indstr",
                                          image = "https://yt3.ggpht.com/-04uuTMHfDz4/AAAAAAAAAAI/AAAAAAAAAAA/Kjeupp-eNNg/s100-c-k-no-rj-c0xffffff/photo.jpg"),
                         sidebarMenu(
-                          menuItem("Filings", tabName = "filings", icon = icon("map")),
+                          menuItem("Filings by year", tabName = "filings", icon = icon("bar-chart")),
                           menuItem("Financial St. Data", tabName = "columns", icon = icon("bar-chart")),
                           menuItem("Scatter by Co", tabName = "scatter", icon = icon("database"))
                         )
                       ),
+                      
                       dashboardBody(
                         tags$head(
                           tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
                         ),
+                        
                         tabItems(
                           tabItem(tabName = "filings",
-                                  plotOutput("gg1"),
+                                  h1(plotOutput('gg1')),
                                   plotOutput("gg2"),
-                                  fluidRow(infoBoxOutput("maxBox"),
-                                           infoBoxOutput("minBox"),
-                                           infoBoxOutput("avgBox")),
+                                  plotOutput("gg3")
+                                  ), #end filings
                                   
+                          
+
                                   tabItem(tabName = "columns",
+                                          selectizeInput("selected",
+                                                         "Select Industry to Display",
+                                                         choice, selected = ''),
+                                          selectizeInput("fin_metr",
+                                                         "Select Line Item to Display",
+                                                         fin_choice, selected = 'Assets')
                                           
-                                          plotOutput("hist")),
-                                  
-                                  
+  
+                                          ), #end columns
                                   
                                   tabItem(tabName = "scatter",
-                                          fluidRow(box(DT::dataTableOutput("table"), width = 12)))
+                        
+                                          selectizeInput("selected",
+                                                         "Select Item to Display",
+                                                         choice, selected = 'total'),
+                                          selectizeInput("fin_metr",
+                                                         "Select Line Item to Display",
+                                                         fin_choice, selected = 'Assets')) #end scatter
+                          ) 
                           )
                         )
-                      )
-))
+)
