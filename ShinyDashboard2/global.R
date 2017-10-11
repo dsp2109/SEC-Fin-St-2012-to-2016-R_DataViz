426d251caa40e0335179a2854e78d7c69a2bd3a1
Daniel Park <dsp2109@gmail.com>
2017-10-11 04:49:14 -0400

    moved all panes to Shinydash2

--- ShinyDashboard2/global.R
+++ ShinyDashboard2/global.R
@@ -8,8 +8,10 @@ library(dplyr)
 
 load("Data/shiny_sec_data.rda")
 load("Data/ggfilings.rda")
-rm(state_stats)
 
-choice = unique(nums_shiny$AD_Desc)
+rownames(state_stats) <- NULL
+choice <- colnames(state_stats)[4:6]
+
+ind_choice = unique(nums_shiny$AD_Desc)
 fin_choice = unique(nums_shiny$consol_name)
-
+fin_st_choice <- c(NULL, unique(state_stats$consol_name))
--- ShinyDashboard2/ui.R
+++ ShinyDashboard2/ui.R
@@ -9,9 +9,14 @@ shinyUI(dashboardPage(
                         sidebarUserPanel("Fin St by: Yr, Indstr",
                                          image = "https://yt3.ggpht.com/-04uuTMHfDz4/AAAAAAAAAAI/AAAAAAAAAAA/Kjeupp-eNNg/s100-c-k-no-rj-c0xffffff/photo.jpg"),
                         sidebarMenu(
+                          h4("Public Financial Statements"),
                           menuItem("Filings by year", tabName = "filings", icon = icon("bar-chart")),
                           menuItem("Financial St. Data", tabName = "columns", icon = icon("bar-chart")),
-                          menuItem("Scatter by Co", tabName = "scatter", icon = icon("line-chart"))
+                          menuItem("Scatter by Co", tabName = "scatter", icon = icon("line-chart")),
+                          h4("By State"),
+                          menuItem("Map", tabName = "map", icon = icon("map")),
+                          menuItem("Histogram", tabName = "histogram", icon = icon("bar-chart")),
+                          menuItem("Data", tabName = "data", icon = icon("database"))
                         )
                       ),
                       
@@ -50,7 +55,35 @@ shinyUI(dashboardPage(
                                                          "Select Line Item to Display",
                                                          fin_choice, selected = 'Assets'),
                                           plotOutput("scat_gg")
-                                          ) #end scatter
+                                          ), #end scatter
+                          
+                          ############ by state tabs
+                            tabItem(tabName = "map",
+                                    selectizeInput("selected",
+                                                   "Select Item to Display",
+                                                   choice, selected = 'total'),
+                                    selectizeInput("fin_metr",
+                                                   "Select Line Item to Display",
+                                                   fin_choice, selected = 'Assets'),
+                                    fluidRow(infoBoxOutput("maxBox"),
+                                             infoBoxOutput("minBox"),
+                                             infoBoxOutput("avgBox")),
+                                    fluidRow(box(htmlOutput("map"), height = 500, width = 12))
+                                    ),
+                            
+                            tabItem(tabName = "histogram",
+                                    selectizeInput("selected",
+                                                   "Select Item to Display",
+                                                   choice, selected = 'total'),
+                                    selectizeInput("fin_metr",
+                                                   "Select Line Item to Display",
+                                                   fin_st_choice, selected = 'Assets'),
+                                    plotOutput("hist")),
+                            # for gvis: fluidRow(box(htmlOutput("hist"), height = 300, width = 12))), #end of 2nd tab
+                            tabItem(tabName = "data",
+                                    fluidRow(box(DT::dataTableOutput("table"), width = 12)))
+                          
+                          
                           ) 
                           )
                         )
